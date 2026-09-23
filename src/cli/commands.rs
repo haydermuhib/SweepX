1ad24a3dc7243e2a483fefcacf9159c5b6d9cd66
Haider Ali <ha344563219@gmail.com>
2026-09-23 22:23:55 +0500

    chore: bump version to v0.1.1

--- src/cli/commands.rs
+++ src/cli/commands.rs
@@ -3,7 +3,7 @@ use clap::{Parser, Subcommand, ValueEnum};
 #[derive(Parser, Debug)]
 #[command(name = "sweepx")]
 #[command(author = "SweepX Contributors")]
-#[command(version = "0.1.0")]
+#[command(version)]
 #[command(about = "Unified Linux Application Tracker & Deep Uninstaller", long_about = None)]
 pub struct Cli {
     #[command(subcommand)]
